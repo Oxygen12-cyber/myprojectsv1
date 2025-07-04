e76906a4e4bab7f26142c855b0c37dd9f4733fe3
Oxygen12-cyber <adeolaodunjo32@gmail.com>
2025-07-04 23:39:15 +0100

    updated login auth code: -added storage function -create account function added too -better email-validation function

--- loginAuth.py
+++ loginAuth.py
@@ -3,31 +3,53 @@ import csv
 
 
 def main():
+    def create_account():
+        global U_name, E_mail, P_ssword
+        print('Creating Account....')
+        U_name = str(input('Your Username: '))
+        E_mail = str(input('Your Email: '))
+        P_ssword = str(input('Your Password: '))
+
     def validate_email(email):
-        
         pattern = r'^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$'
-        return re.match(pattern, email) is not None
-    
+        if re.match(pattern, email):
+            return email
+        else:
+            raise Exception('Wrong Email format!!!!')
+        
     def validate_password(password):
-        pass
+        return password
+
+    def store_details():
+        data = [U_name, validate_email(E_mail), validate_password(P_ssword)]
+        header = ['Username', 'Email', 'Password']
+        with open('userdetails.csv', '+a') as file:
+            writer = csv.writer(file)
+            writer.writerow(data)
 
     class User:
         def __init__(self, username: str, email: str, password: str) -> None:
             self.username = username
-            self.email = validate_email(email)
-            self.password = validate_password(password)
+            self.email = email
+            self._password = password
         
-        def _change_username(self, new_username: str) -> None:
+        def change_username(self, new_username: str) -> None:
             self.username = new_username
 
-        def _change_password(self):
-            print('changeing passwords......')
+        def change_password(self):
+            print('changing passwords......')
 
-        def _check_password(self):
-            return self.password
+        def check_password(self):
+            return self._password
+        
+    # Running functions
+    create_account()
+    store_details()
 
     # Usage for Testing
-    user1 = User('james01', 'james@py.com', 'pass123456')
+    user1 = User(U_name, validate_email(E_mail), validate_password(P_ssword))
+    print(user1.username)
+    print(user1.email)
 
 
 if __name__ == "__main__":
---
+++ testingcode.py
@@ -0,0 +1,12 @@
+import re
+
+
+
+n = 'james'
+m = 'peter'
+j = 'yoyo'
+
+data = [n, m, j]
+print(data)
+
+mail = 'adeola-odunjo32@gmail.com'
